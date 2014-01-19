5cf6e0775d51917630cbf5e81da90c28178e8f00
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2014-01-19 05:01:02 +0100

    Game selection and launching

--- source/views/Home.d
+++ source/views/Home.d
@@ -1,4 +1,5 @@
 import std.stdio;
+import std.conv;
 
 import AGSEvent;
 import View;
@@ -13,20 +14,31 @@ class Home : View {
 
 		m_font = new Font;
 		m_font.loadFromFile("res/title.ttf");
-		m_gamename = new Text("GG HF GL WP", m_font, 60);
+		m_gamename = new Text(to!dstring(m_games[m_nGameIndex].GetName()), m_font, 60);
 		m_gamename.setColor(Color(0,0,0));
 		m_gamename.position(Vector2f(70,20));
 	}
 
 	override void OnEvent(in AGSEvent e)
 	{
-		writeln("Event received: ",e);
-		//if(e == AGSEvent.JoyUp)
-		//{
-		//	m_nGameIndex--;
-		//	if(m_nGameIndex<0)
-		//		m_nGameIndex=0;
-		//}
+		if(e == AGSEvent.AGSEvent.JoyUp)
+		{
+			if(m_nGameIndex>0)
+				m_nGameIndex--;
+
+			m_gamename.setString(to!dstring(m_games[m_nGameIndex].GetName()));
+		}
+		else if(e == AGSEvent.AGSEvent.JoyDown)
+		{
+			if(m_nGameIndex+1<m_games.length)
+				m_nGameIndex++;
+
+			m_gamename.setString(to!dstring(m_games[m_nGameIndex].GetName()));
+		}
+		else if(e == AGSEvent.AGSEvent.ButStart)
+		{
+			m_games[m_nGameIndex].Start();
+		}
 	}
 
 	override void Render(ref RenderWindow win)
@@ -41,7 +53,7 @@ private:
 	GameList m_gl;
 	Game[] m_games;
 
-	int m_nGameIndex;
+	size_t m_nGameIndex;
 
 
 }
