cd51219f78a820748a571fbbe50b98aa48d200e0
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2014-01-23 01:37:38 +0100

    Enhanced game browsing

--- source/View.d
+++ source/View.d
@@ -3,6 +3,9 @@ import dsfml.graphics;
 
 interface View
 {
+	static immutable int viewwidth=800;
+	static immutable int viewheight=600;
+
     void Render(ref RenderWindow win);
 
     void OnEvent(in AGSEvent e);
--- source/views/Home.d
+++ source/views/Home.d
@@ -5,6 +5,7 @@ import AGSEvent;
 import View;
 import dsfml.graphics;
 import GameList;
+import AlignedText;
 import ButtonsLayout;
 
 class Home : View {
@@ -13,16 +14,38 @@ class Home : View {
 		m_games = m_gl.GetGames();
 		m_nGameIndex = 0;
 
-		m_font = new Font;
-		m_font.loadFromFile("res/title.ttf");
-		m_gamename = new Text(to!dstring(m_games[m_nGameIndex].GetName()), m_font, 60);
-		m_gamename.setColor(Color(0,0,0));
-		m_gamename.origin(Vector2f(m_gamename.getLocalBounds().width/2., m_gamename.getLocalBounds().height/2.));
-		m_gamename.position(Vector2f(800/2,50));
+		m_fontTitle = new Font;
+		m_fontTitle.loadFromFile("res/title.ttf");
+		m_fontText = new Font;
+		m_fontText.loadFromFile("res/default.ttf");
+		m_texArrow = new Texture;
+		m_texArrow.loadFromFile("res/ArrowActive.png");
 
-		m_blPlay = new ButtonsLayout(
-			AGSEvent.AGSEvent.ButA
-			, "Jouer");
+		m_sprArrow[0] = new Sprite(m_texArrow);
+		m_sprArrow[0].origin(Vector2f(20,10));
+		m_sprArrow[0].position(Vector2f(viewwidth-50,55));
+		m_sprArrow[1] = new Sprite(m_texArrow);
+		m_sprArrow[1].origin(Vector2f(20,10));
+		m_sprArrow[1].rotation(180);
+		m_sprArrow[1].position(Vector2f(viewwidth-50,80));
+
+		m_gamename[0] = new AlignedText(" ", m_fontTitle, 30);
+		m_gamename[0].setColor(Color(64,64,64));
+		m_gamename[0].alignment(AlignedText.AlignedText.Alignment.right);
+		m_gamename[0].position(Vector2f(viewwidth-50,25));
+		m_gamename[1] = new AlignedText(" ", m_fontTitle, 45);
+		m_gamename[1].setColor(Color(0,0,0));
+		m_gamename[1].alignment(AlignedText.AlignedText.Alignment.center);
+		m_gamename[1].position(Vector2f(viewwidth/2,60));
+		m_gamename[2] = new AlignedText(" ", m_fontTitle, 30);
+		m_gamename[2].setColor(Color(64,64,64));
+		m_gamename[2].alignment(AlignedText.AlignedText.Alignment.right);
+		m_gamename[2].position(Vector2f(viewwidth-50,105));
+
+		UpdateGameNames();
+
+
+		m_blPlay = new ButtonsLayout(AGSEvent.AGSEvent.ButA, "Jouer");
 		m_blPlay.position(Vector2f(800/2,600-100));
 	}
 
@@ -32,17 +55,13 @@ class Home : View {
 		{
 			if(m_nGameIndex>0)
 				m_nGameIndex--;
-
-			m_gamename.setString(to!dstring(m_games[m_nGameIndex].GetName()));
-			m_gamename.origin(Vector2f(m_gamename.getLocalBounds().width/2, m_gamename.getLocalBounds().height/2));
+			UpdateGameNames();
 		}
 		else if(e == AGSEvent.AGSEvent.JoyDown)
 		{
 			if(m_nGameIndex+1<m_games.length)
 				m_nGameIndex++;
-
-			m_gamename.setString(to!dstring(m_games[m_nGameIndex].GetName()));
-			m_gamename.origin(Vector2f(m_gamename.getLocalBounds().width/2, m_gamename.getLocalBounds().height/2));
+			UpdateGameNames();
 		}
 		else if(e == AGSEvent.AGSEvent.ButA)
 		{
@@ -52,15 +71,39 @@ class Home : View {
 
 	override void Render(ref RenderWindow win)
 	{
-		win.draw(m_gamename);
+		foreach(ref gamename ; m_gamename)
+			win.draw(gamename);
+		
+		if(m_nGameIndex>0)
+			win.draw(m_sprArrow[0]);
+		if(m_nGameIndex<m_games.length-1)
+			win.draw(m_sprArrow[1]);
+
 		win.draw(m_blPlay);
 
 	}
 
 private:
-	Font m_font;
-	Text m_gamename;
+	void UpdateGameNames()
+	{
+		if(m_nGameIndex>=1)
+			m_gamename[0].setString(to!dstring(m_games[m_nGameIndex-1].GetName()));
+		else
+			m_gamename[0].setString(" ");
+
+		m_gamename[1].setString(to!dstring(m_games[m_nGameIndex].GetName()));
+
+		if(m_nGameIndex<=m_games.length-2)
+			m_gamename[2].setString(to!dstring(m_games[m_nGameIndex+1].GetName()));
+		else
+			m_gamename[2].setString(" ");
+	}
+
+	Font m_fontTitle, m_fontText;
+	AlignedText m_gamename[3];
 	ButtonsLayout m_blPlay;
+	Texture m_texArrow;
+	Sprite m_sprArrow[2];
 
 	GameList m_gl;
 	Game[] m_games;
